9c81ecdce725f1edd063df09aba7cae8b8bfec28
shaylevy01 <154368549+shaylevy01@users.noreply.github.com>
2023-12-19 21:47:17 +0200

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -5,6 +5,15 @@
  */ 
 public class Triangle {
 	public static void main(String[] args) {
-		// Put your code here	
+		//Receives three numbers, checks if they can form a triangle and prints the result.
+        	int tside1 = Integer.parseInt(args[0]);
+		int tside2 = Integer.parseInt(args[1]);
+		int tside3 = Integer.parseInt(args[2]);
+		if ((tside1 + tside2 > tside3) && (tside1 + tside3 > tside2) && (tside2 + tside3 > tside1)) {
+			System.out.println(tside1 + ", " + tside2 + ", " + tside3 + ": true");
+		}
+		else {
+			System.out.println(tside1 + ", " + tside2 + ", " + tside3 + ": false");
+		}	
 	}
 }
