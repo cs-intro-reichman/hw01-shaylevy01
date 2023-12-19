79f2cf9e158298c9ad31882f6a3f01812921258e
shaylevy01 <154368549+shaylevy01@users.noreply.github.com>
2023-12-19 21:44:58 +0200

    Update Coins.java

--- Coins.java
+++ Coins.java
@@ -4,6 +4,10 @@
  */
 public class Coins {
 	public static void main(String[] args) {
-		// Put your code here
+		//Receives a number of cents and prints the representation of them in quarters and cents using as many quarters as possible.
+        	int cents = Integer.parseInt(args[0]);
+		int quarter = cents / 25;
+		int remnants = cents % 25;
+		System.out.println("Use " + quarter + " quarters and " + remnants + " cents");
 	}
 }
